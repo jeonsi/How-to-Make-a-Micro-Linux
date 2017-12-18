61e9aac29f37665cb0a915871d9c785244b6e3eb
Seung-il Jeon <si.j@navercorp.com>
2017-12-18 18:17:14 +0900

    update

--- src/packages/rootfs/etc/bootscript.sh
+++ src/packages/rootfs/etc/bootscript.sh
@@ -5,6 +5,6 @@ mount -t proc none /proc
 mount -t sysfs none /sys
 
 for DEVICE in /sys/class/net/* ; do
-    ip link set \${DEVICE##*/} up
-    [ \${DEVICE##*/} != lo ] && udhcpc -b -i \${DEVICE##*/} -s /etc/rc.dhcp
+    [ ${DEVICE##*/} != sit0 ] && ip link set ${DEVICE##*/} up
+    [ ${DEVICE##*/} != lo ] && [ ${DEVICE##*/} != sit0 ] && udhcpc -b -i ${DEVICE##*/} -s /etc/rc.dhcp
 done
