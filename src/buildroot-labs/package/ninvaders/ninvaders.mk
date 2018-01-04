03ec77617a72d4003c4fc7e886b277f5ff595700
Seung-il Jeon <si.j@navercorp.com>
2018-01-04 10:04:24 +0900

    added buildroot-labs

---
+++ src/buildroot-labs/package/ninvaders/ninvaders.mk
@@ -0,0 +1,19 @@
+###############################################################################$
+#
+# ninvaders
+#
+################################################################################
+
+NINVADERS_VERSION = 0.1.1
+NINVADERS_SITE = http://downloads.sourceforge.net/project/ninvaders/ninvaders/0.1.1
+NINVADERS_DEPENDENCIES = ncurses
+
+define NINVADERS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define NINVADERS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/nInvaders $(TARGET_DIR)/usr/bin/nInvaders
+endef
+
+$(eval $(generic-package))
